9c2178859316546b3b83cdb9b71223152afde31e
Popa Teodor <teodor.popa96@gmail.com>
2024-04-16 13:53:36 +0300

    cookies

--- phpDir/src/PHP_practice01/9.php
+++ phpDir/src/PHP_practice01/9.php
@@ -17,16 +17,39 @@
   <article class="main-content col-xs-8">
 
 
+  <a href="9.php?source=32332">Click here</a>
 
     <?php
 
-		/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
+		/* Create a link saying Click Here, and set the link href to pass some parameters and use the GET super global to see it
 
-		Step 2 - Set a cookie that expires in one week
 
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
+
+		 Step 2 - Set a cookie that expires in one week
+
+		 Step 3 - Start a session and set it to value, any value you want. */
+
+     if(isset($_GET["source"])) {
+      echo $_GET["source"];
+     }
+
+     $do_remember = isset($_POST['remember']);
+     // Use a timespan of 1 week
+
+     $remembering_timespan = time() + 7 * 24 * 60 * 60;
+     if ($do_remember)
+     {
+      setcookie("remember_me", $_POST["username"], $remembering_timespan);
+     }
+
+  $_SESSION["favcolor"] = "green";
+  $_SESSION["favanimal"] = "cat";
+
+  echo "Session variables are set";
+
+ 
+
+ 	
 
 		?>
 
